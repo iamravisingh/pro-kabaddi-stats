d90c1e3b6b216037c34da7900b7827008097e2d9
Ravi Singh <ravi.u.singh@here.com>
2024-11-09 19:27:49 +0530

    initial setup, added react, deckgl and maplibre to support base map

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import MapComponent from "./components/Map";
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <MapComponent/>
+      </div>
+    </>
+  );
+}
+
+export default App;
